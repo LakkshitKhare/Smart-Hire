83a41806d3be4cabf0d04e0ebfb850a8bece3937
LakkshitKhare <lakkshitkhare@gmail.com>
2026-05-24 20:42:46 +0530

    File Structure

---
+++ backend/src/test/java/com/project/smarthire/SmarthireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.smarthire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmarthireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
